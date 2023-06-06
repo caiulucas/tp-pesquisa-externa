919b7ff36563aa22b5ef74b75aa94b63e4e2b5af
Caio Lucas <lienscarlet@gmail.com>
2023-06-06 11:21:48 -0300

    feat: implement indexed sequential search

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "fileHandler.h"
-#include "sequencialIndexado.h"
+#include "indexed.h"
 #include "sizes.h"
 
 // Indexada
@@ -9,17 +9,44 @@
 
 int main()
 {
-  //printBinaryFile("data.bin");
   FILE *file = fopen("data.bin", "rb");
 
+  if(file == NULL)
+  {
+    printf("Erro ao abrir o arquivo\n");
+    return 1;
+  }
 
-  tipoindice tab[ITENSPAGINA];
-  Data *data;
-  data->key = 9;
+  printBinaryFile(file);
+  printf("-------------------------\n");
+  fseek(file, 0, SEEK_SET);
 
-  int gotit = pesquisa(tab, 100, data, file);
-  printf("%d", gotit);
+  Index indexes[INDEXES_MAX];
+  Data item;
   
+  int count = 0;
+  int pos = 0;
+
+  while(fread(&item, sizeof(Data), 1, file) == 1) {
+    count++;
+    if(count % PAGE_ITEMS == 1){
+      indexes[pos].key = item.key;
+      indexes[pos].pos = pos + 1;
+      pos++;
+    }
+  }
+
+  fflush(stdout);
+  printf("Digite a chave do item que deseja buscar: ");
+  scanf("%d", &item.key);
+
+  if(search(indexes, pos, &item, file)) {
+    printf("Item encontrado: %d %ld\n", item.key, item.data1);
+  }
+  else {
+    printf("Item não encontrado\n");
+  }
+
   fclose(file);
   return 0;
 }
