5dfedfe341245f278fce6a4b9876dcd958f67394
Caio Lucas <lienscarlet@gmail.com>
2023-06-27 09:21:08 -0300

    comment binary tree

--- binary_tree.c
+++ binary_tree.c
@@ -9,6 +9,7 @@ void startBinaryTree(Node **root)
   *root = NULL;
 }
 
+// Cria um novo nó
 Node *createNode(Data data, size_t pos)
 {
   Node *node = (Node *)malloc(sizeof(Node));
@@ -24,12 +25,15 @@ bool insertBinaryTree(Data data, FILE *binaryTreeFile)
   if (!binaryTreeFile)
     return false;
 
+  // Cria um nó auxiliar para percorrer a árvore
   Node aux = {data.key, -1, -1, 0};
 
+  // Pega a quantidade de nós no arquivo
   fseek(binaryTreeFile, 0, SEEK_END);
   size_t length = ftell(binaryTreeFile) / sizeof(Node);
   rewind(binaryTreeFile);
 
+  // Se o arquivo estiver vazio, insere o nó na raiz
   if (length == 0)
   {
     Node *newNode = createNode(data, length);
@@ -38,44 +42,56 @@ bool insertBinaryTree(Data data, FILE *binaryTreeFile)
     return true;
   }
 
+  // Percorre a árvore até encontrar a posição correta para inserir o nó
   while (fread(&aux, sizeof(Node), 1, binaryTreeFile) == 1)
   {
     size_t displacement;
 
+    // Se a chave for menor que a chave do nó atual, vai para a esquerda
     if (data.key < aux.key)
     {
+      // Se não houver um nó a esquerda, insere o nó
       if (aux.leftNode == -1)
       {
         aux.leftNode = length;
+        // Volta para o nó anterior e atualiza o nó
         fseek(binaryTreeFile, -sizeof(Node), SEEK_CUR);
         fwrite(&aux, sizeof(Node), 1, binaryTreeFile);
         break;
       }
+      // Se houver um nó a esquerda, vai para ele
       displacement = (size_t)(aux.leftNode) * sizeof(Node);
     }
     else if (data.key > aux.key)
     {
+      // Se não houver um nó a direita, insere o nó
       if (aux.rightNode == -1)
       {
         aux.rightNode = length;
+        // Volta para o nó anterior e atualiza o nó
         fseek(binaryTreeFile, -sizeof(Node), SEEK_CUR);
         fwrite(&aux, sizeof(Node), 1, binaryTreeFile);
         break;
       }
 
+      // Se houver um nó a direita, vai para ele
       displacement = (size_t)(aux.rightNode) * sizeof(Node);
     }
+    // Se a chave for igual a chave do nó atual, retorna falso
     else
     {
       rewind(binaryTreeFile);
       return false;
     }
 
+    // Vai para o nó
     fseek(binaryTreeFile, displacement, SEEK_SET);
   }
 
+  // Cria um novo nó e insere no arquivo
   Node *newNode = createNode(data, length);
 
+  // Vai para o final do arquivo e insere o nó
   fseek(binaryTreeFile, 0, SEEK_END);
   fwrite(newNode, sizeof(Node), 1, binaryTreeFile);
   rewind(binaryTreeFile);
@@ -85,16 +101,20 @@ bool insertBinaryTree(Data data, FILE *binaryTreeFile)
 
 bool findBinaryTree(Data *data, FILE *dataFile, Quantifier *quantifier)
 {
+  // Abre o arquivo da árvore binária
   FILE *binaryTreeFile = fopen(BINARY_TREE_FILE, "rb");
 
   if (!binaryTreeFile)
     return false;
 
+  // Cria um nó auxiliar para percorrer a árvore
   Node aux;
   while (fread(&aux, sizeof(Node), 1, binaryTreeFile) == 1)
   {
+    // Se a chave for igual a chave do nó atual, retorna verdadeiro
     if (data->key == aux.key)
     {
+      // Vai para a posição do registro no arquivo de dados
       fseek(dataFile, aux.pos * sizeof(Data), SEEK_SET);
       fread(data, sizeof(Data), 1, dataFile);
       quantifier->reads += 1;
@@ -107,12 +127,14 @@ bool findBinaryTree(Data *data, FILE *dataFile, Quantifier *quantifier)
 
     size_t displacement;
 
+    // Se a chave for menor que a chave do nó atual, vai para a esquerda
     quantifier->comparisons += 1;
     if (data->key > aux.key)
       displacement = (size_t)(aux.rightNode) * sizeof(Node);
-    else
+    else // Se a chave for maior que a chave do nó atual, vai para a direita
       displacement = (size_t)(aux.leftNode) * sizeof(Node);
 
+    // Vai para o nó
     fseek(binaryTreeFile, displacement, SEEK_SET);
     quantifier->reads += 1;
     }
@@ -132,6 +154,8 @@ void createBinaryTree(FILE *dataFile)
 
   rewind(dataFile);
   int i = 1;
+
+  // Lê os registros do arquivo de dados e insere na árvore binária
   while (fread(&data, sizeof(Data), 1, dataFile) == 1)
   {
     clear();
@@ -173,6 +197,7 @@ bool runBinaryTree(Input input, FILE *dataFile)
 
   item.key = input.key;
 
+  // Procura o item na árvore binária
   if (findBinaryTree(&item, dataFile, &quantifier))
   {
     printf("[SUCCESS] Item encontrado!\n");
